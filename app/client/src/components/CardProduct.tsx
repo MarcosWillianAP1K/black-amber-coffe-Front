1f656a465898a22aa2aefe85649edd52b150c928
MarcosWillianAP1K <mwss@outlook.com>
2026-06-06 15:41:39 -0300

    feat: update CardProduct component to handle optional imageUrl and improve layout; enhance ButtonAddProduct styling

--- app/client/src/components/CardProduct.tsx
+++ app/client/src/components/CardProduct.tsx
@@ -5,7 +5,7 @@ export interface CardProductProps {
     itens: string;
     price: number;
     promotionPrice?: number;
-    imageUrl: string;
+    imageUrl?: string;
     onClick: () => void;
 }
 
@@ -16,24 +16,37 @@ export interface CardProductProps {
 export function CardProduct({ name, itens, price, promotionPrice, imageUrl, onClick }: CardProductProps) {
 
     return (
-        <div className="w-52 h-auto p-4 bg-(--Widget-background) rounded-lg flex flex-col items-center justify-center gap-4 shadow-lg">
-            <img src={imageUrl} alt={name} className="w-40 h-40 object-cover rounded-md" />
+        <div className="w-36 h-fit p-4 bg-(--Widget-background) rounded-sm flex flex-col items-center justify-center gap-2 shadow-lg">
+            {imageUrl && (
+                <img
+                    src={imageUrl}
+                    alt={name}
+                    className="w-full h-26 object-cover rounded-sm overflow-hidden"
+                />
+            )}
 
-            <div className="flex flex-col items-start gap-2 w-full">
+            {!imageUrl && (
+                <div className="w-full h-26 bg-gray-300 rounded-sm flex items-center justify-center">
+                    <span className="text-gray-500 text-sm">Imagem não disponível</span>
+                </div>
+            )}
 
-                <h2 className="text-lg font-bold text-white">{name}</h2>
-                <p className="text-(--Primary-off) text-sm font-secondary overflow-hidden text-ellipsis line-clamp-2">{itens}</p>
+
+            <div className="w-full h-full flex flex-col items-start gap-2">
+
+                <h2 className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-[14px] font-bold text-white">{name}</h2>
+                <p className="text-(--Primary-off) text-[8px] font-secondary overflow-hidden text-ellipsis line-clamp-2">{itens}</p>
                 
-                <div className="flex items-center justify-between w-full">
-                    <div className="flex items-center gap-2">
+                <div className="w-full h-fit flex flex-row items-center justify-between gap-0">
+                    <div className=" flex items-center gap-1 whitespace-nowrap">
                         {promotionPrice !== undefined && (
-                            <span className="text-base font-bold text-yellow-400">R$ {promotionPrice.toFixed(2)}</span>
+                            <span className="text-[12px] font-bold text-yellow-400">R$ {promotionPrice.toFixed(2)}</span>
                         )}
-                        <span className={`text-base font-bold ${promotionPrice !== undefined ? 'text-gray-400 line-through' : 'text-green-400'}`}>
+                        <span className={`text-[8px] font-bold ${promotionPrice !== undefined ? 'text-gray-400 line-through' : 'text-green-400'}`}>
                             R$ {price.toFixed(2)}
                         </span>
                     </div>
-
+                        
                     <ButtonAddProduct onClick={onClick} />
                 </div>
             </div>
--- app/client/src/pages/content/Home.tsx
+++ app/client/src/pages/content/Home.tsx
@@ -4,10 +4,13 @@ import { CardProduct } from "../../components/CardProduct";
 export function Home() {
 
     return (
-        <div className="w-full h-full flex items-center justify-center">
+        <div className="w-full h-full gap-4 flex flex-col items-center justify-center">
             <h1 className=" text-white text-2xl font-bold">
                 Bem-vindo ao Black Amber Coffee!
             </h1>
+
+
+            <div className="w-full h-fit gap-4 flex flex-wrap items-center justify-center">
 
             <CardProduct
                 name="Café Expresso"
@@ -17,7 +20,29 @@ export function Home() {
                 imageUrl="https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FmJTIwZXhwcmVzc298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
                 onClick={() => alert("Produto adicionado ao carrinho!")}
             />
+
+            <CardProduct
+                name="Café Expresso"
+                itens="Grãos selecionados, torra média, sabor intenso e aroma marcante."
+                price={9.99}
+                promotionPrice={7.99}
+                imageUrl="https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FmJTIwZXhwcmVzc298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+                onClick={() => alert("Produto adicionado ao carrinho!")}
+            />
+
+            <CardProduct
+                name="Café Expresso"
+                itens="Grãos selecionados, torra média, sabor intenso e aroma marcante."
+                price={9.99}
+                promotionPrice={7.99}
+                imageUrl="https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FmJTIwZXhwcmVzc298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+                onClick={() => alert("Produto adicionado ao carrinho!")}
+            />
+
+
+
         </div>
+        </div >
     );
 
 }
